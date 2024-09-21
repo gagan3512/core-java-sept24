f0e622d19b80c0fc07c0a05f9383159fd72e4c91
sunnycode07 <sunny@pragra.io>
2024-09-21 13:27:32 -0400

    Scanner

---
+++ src/sep21/calc/Calculator.java
@@ -0,0 +1,18 @@
+package sep21.calc;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("****************");
+        System.out.println("Welcome to My calcualtor");
+        System.out.println("****************");
+        System.out.println("1. Addition ");
+        System.out.println("2. Subtraction ");
+        System.out.println("3. Multiplication ");
+        System.out.println("4. Division ");
+        System.out.println("5. Modulus ");
+        System.out.println("0. exit.  ");
+
+
+    }
+}
